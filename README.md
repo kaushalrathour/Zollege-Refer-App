c2bd32064594cb06c2b252892f5e7555ef981821
Kaushal Rathour <kaushallsbd@gmail.com>
2024-12-21 10:59:14 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# **Refer App for Zollege**
---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <h1>Zollege Refer App</h1>
+    </React.Fragment>
+  );
+}
