6bcfdbd3e606ed45121361c41a31e726bf3d57fd
Kaushal Rathour <kaushallsbd@gmail.com>
2024-12-23 15:26:51 +0530

    Replaced Credentials For Production

--- client/src/features/endpointSlice.js
+++ client/src/features/endpointSlice.js
@@ -1,10 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const TESTING_API_ENDPOINT = "http://localhost:5000";
-// const PRODUCTION_API_ENDPOINT = "";
+// const TESTING_API_ENDPOINT = "http://localhost:5000";
+const PRODUCTION_API_ENDPOINT = "https://zollege-refer-app.onrender.com";
 
 const initialState = {
-  endpoint: TESTING_API_ENDPOINT,
+  // endpoint: TESTING_API_ENDPOINT,
+  endpoint: PRODUCTION_API_ENDPOINT,
 };
 const endpointSlice = createSlice({
   name: "endpoint",
